7295fe1a5b7ae621f4bedc90418303384deb4b6c
MatthewLeaman <leaman.30@osu.edu>
2024-12-15 02:24:36 -0500

    complete

--- server/app.py
+++ server/app.py
@@ -18,6 +18,7 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -87,12 +88,25 @@ class CheckSession(Resource):
 class MemberOnlyIndex(Resource):
     
     def get(self):
-        pass
+        
+        if not session['user_id']:
+            return {'error': 'Unauthorized'}, 401
+        else:
+            articles = Article.query.filter_by(is_member_only = True).all()
+            articles_dict = [article.to_dict() for article in articles]
+            return articles_dict,200
+
+        
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
+        if not session['user_id']:
+            return {'error': 'Unauthorized'}, 401
+        else:
+            articles = Article.query.filter(Article.id == id).first()
+            return articles.to_dict(), 200
+        
 
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
